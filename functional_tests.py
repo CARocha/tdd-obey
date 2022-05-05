905516a5c6851d81ff2201520ca7e10b48dd2c74
Carlos Rocha <crocha09.09@gmail.com>
2022-05-05 11:22:25 -0600

    fix deprecation warning selenium driver service

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,14 @@
-from selenium import webdriver
+from django.test import LiveServerTestCase
 
-browser = webdriver.Firefox()
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.firefox import GeckoDriverManager
+
+#driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+
+browser = driver
 browser.get("http://localhost:8000")
 
-assert "Django" in browser.title
+assert "The install worked successfully! Congratulations!" in browser.title
